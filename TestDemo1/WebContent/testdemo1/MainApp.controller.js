3ebd14544bd4ac42d3d4ccd219710a7a2756a9c9
PhilippG <philipp.gembe@ciber.com>
2014-12-02 12:52:06 +0100

    Addes some Table controls to demonstrate the issue regarding scrolling and data binding using type system.

--- TestDemo1/WebContent/testdemo1/MainApp.controller.js
+++ TestDemo1/WebContent/testdemo1/MainApp.controller.js
@@ -5,9 +5,15 @@ sap.ui.controller("testdemo1.MainApp", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf testdemo1.MainApp
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+		
+		var aData = jsonData.Rowsets.Rowset[0].Row;
+
+		var oModel = new sap.ui.model.json.JSONModel(aData);
+		
+		this.getView().setModel(oModel, "DataModel");
+		
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
@@ -36,3 +42,54 @@ sap.ui.controller("testdemo1.MainApp", {
 //	}
 
 });
+
+var jsonData = { "Rowsets" : { 
+	"DateCreated" : "2014-12-02T12:19:58", "Version" : "14.0 SP5 Patch 10 (11.08.2014)", "StartDate" : "2014-12-02T11:19:58", "EndDate" : "2014-12-02T12:19:58", "CachedTime" : "", 
+	"Rowset" : [ 
+	{ "Columns" : { 
+		"Column" : [ 
+		{ "Name" : "TOP_ID", "SourceColumn" : "TOP_ID", "Description" : "TOP_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "FQN", "SourceColumn" : "FQN", "Description" : "FQN", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "NAME", "SourceColumn" : "NAME", "Description" : "NAME", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "WORKPLACE_ID", "SourceColumn" : "WORKPLACE_ID", "Description" : "WORKPLACE_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "ACT_ID", "SourceColumn" : "ACT_ID", "Description" : "ACT_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "ACT_ST", "SourceColumn" : "ACT_ST", "Description" : "ACT_ST", "SQLDataType" : -101, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "ACT_DESCRP", "SourceColumn" : "ACT_DESCRP", "Description" : "ACT_DESCRP", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "AUF_NR", "SourceColumn" : "AUF_NR", "Description" : "AUF_NR", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "REF_ID", "SourceColumn" : "REF_ID", "Description" : "REF_ID", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "MAT_NR", "SourceColumn" : "MAT_NR", "Description" : "MAT_NR", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "MAT_TX", "SourceColumn" : "MAT_TX", "Description" : "MAT_TX", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "CHG_ID", "SourceColumn" : "CHG_ID", "Description" : "CHG_ID", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SEG_ID", "SourceColumn" : "SEG_ID", "Description" : "SEG_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SEG_ST", "SourceColumn" : "SEG_ST", "Description" : "SEG_ST", "SQLDataType" : 12, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "EQI_STA_ID", "SourceColumn" : "EQI_STA_ID", "Description" : "EQI_STA_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SEG_DESCRP", "SourceColumn" : "SEG_DESCRP", "Description" : "SEG_DESCRP", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "EQI_ITM_STA", "SourceColumn" : "EQI_ITM_STA", "Description" : "EQI_ITM_STA", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "EQI_ITM_STA_DESCP", "SourceColumn" : "EQI_ITM_STA_DESCP", "Description" : "EQI_ITM_STA_DESCP", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "EQI_SYS_STA_ID", "SourceColumn" : "EQI_SYS_STA_ID", "Description" : "EQI_SYS_STA_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "EQI_SYS_STA_KEY", "SourceColumn" : "EQI_SYS_STA_KEY", "Description" : "EQI_SYS_STA_KEY", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SUB_ID", "SourceColumn" : "SUB_ID", "Description" : "SUB_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SUB_ST", "SourceColumn" : "SUB_ST", "Description" : "SUB_ST", "SQLDataType" : 12, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "OEE_STA_ID", "SourceColumn" : "OEE_STA_ID", "Description" : "OEE_STA_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "SUB_DESCRP", "SourceColumn" : "SUB_DESCRP", "Description" : "SUB_DESCRP", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "OEE_ITM_STA", "SourceColumn" : "OEE_ITM_STA", "Description" : "OEE_ITM_STA", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "OEE_ITM_STA_DESCP", "SourceColumn" : "OEE_ITM_STA_DESCP", "Description" : "OEE_ITM_STA_DESCP", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "OEE_SYS_STA_ID", "SourceColumn" : "OEE_SYS_STA_ID", "Description" : "OEE_SYS_STA_ID", "SQLDataType" : 2, "MinRange" : 1.0, "MaxRange" : 1.0 }, 
+		{ "Name" : "OEE_SYS_STA_KEY", "SourceColumn" : "OEE_SYS_STA_KEY", "Description" : "OEE_SYS_STA_KEY", "SQLDataType" : -9, "MinRange" : 1.0, "MaxRange" : 1.0 } ] }, 
+	"Row" : [ 
+		{ "TOP_ID" : 911111.00, "FQN" : "91AAAA", "NAME" : "91AAAA", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001751.00, "ACT_ST" : "2014-11-14 16:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002413.00, "SEG_ST" : "2014-11-14 18:00:00 +01:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : "ACT Changed!!", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : 10002095.00, "SUB_ST" : "2014-11-14 18:30:00 +01:00", "OEE_STA_ID" : 911120.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "911120: No Demand  (Don't touch!)", "OEE_ITM_STA_DESCP" : "No Process Orders", "OEE_SYS_STA_ID" : 112.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_DTND" }, 
+		{ "TOP_ID" : 911112.00, "FQN" : "91AAAB", "NAME" : "91AAAB", "WORKPLACE_ID" : 1.00, "ACT_ID" : 10001755.00, "ACT_ST" : "2014-11-05 17:05:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002422.00, "SEG_ST" : "2014-11-06 10:00:00 +01:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : 10002104.00, "SUB_ST" : "2014-11-06 11:00:00 +01:00", "OEE_STA_ID" : 911113.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "911113: Factory Holiday (Don't touch!)", "OEE_ITM_STA_DESCP" : "Factory Holiday", "OEE_SYS_STA_ID" : 111.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_DTPL" }, 
+		{ "TOP_ID" : 911113.00, "FQN" : "91AAAC", "NAME" : "91AAAC", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001630.00, "ACT_ST" : "2014-10-07 06:00:00.0 +2:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : "95030", "MAT_TX" : "IBC                      (BASF)", "CHG_ID" : " ", "SEG_ID" : 10002417.00, "SEG_ST" : "2014-11-12 15:10:00 +01:00", "EQI_STA_ID" : 900140.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "RUN", "EQI_ITM_STA_DESCP" : "Equipment Running", "EQI_SYS_STA_ID" : 40.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_RUN", "SUB_ID" : 10002092.00, "SUB_ST" : "2014-11-12 15:10:00 +01:00", "OEE_STA_ID" : 914000.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "Running (Don't touch!)", "OEE_ITM_STA_DESCP" : "Running", "OEE_SYS_STA_ID" : 400.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_OPER" }, 
+		{ "TOP_ID" : 911114.00, "FQN" : "91AAAD", "NAME" : "91AAAD", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001753.00, "ACT_ST" : "2014-11-11 11:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002412.00, "SEG_ST" : "2014-11-11 11:00:00 +01:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : "Abbruch", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : 10002083.00, "SUB_ST" : "2014-11-11 11:00:00 +01:00", "OEE_STA_ID" : 911110.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "911110: Planned Dowtime (Don't touch!)", "OEE_ITM_STA_DESCP" : "Planned Downtime", "OEE_SYS_STA_ID" : 111.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_DTPL" }, 
+		{ "TOP_ID" : 911118.00, "FQN" : "91AAAH", "NAME" : "91AAAH", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001730.00, "ACT_ST" : "2014-11-03 06:30:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : "P4710", "MAT_TX" : "Produkt 4710 L2", "CHG_ID" : " ", "SEG_ID" : 10002315.00, "SEG_ST" : "2014-11-03 06:30:00 +01:00", "EQI_STA_ID" : 900140.00, "SEG_DESCRP" : "RUN nach RUN aus A", "EQI_ITM_STA" : "RUN", "EQI_ITM_STA_DESCP" : "Equipment Running", "EQI_SYS_STA_ID" : 40.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_RUN", "SUB_ID" : 10002026.00, "SUB_ST" : "2014-11-03 06:30:00 +01:00", "OEE_STA_ID" : 914000.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "Running (Don't touch!)", "OEE_ITM_STA_DESCP" : "Running", "OEE_SYS_STA_ID" : 400.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_OPER" }, 
+		{ "TOP_ID" : 911122.00, "FQN" : "91AABB", "NAME" : "9111BB", "WORKPLACE_ID" : 1.00, "ACT_ID" : 10001665.00, "ACT_ST" : "2014-11-01 00:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002242.00, "SEG_ST" : "2014-11-05 18:15:00 +01:00", "EQI_STA_ID" : 900140.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "RUN", "EQI_ITM_STA_DESCP" : "Equipment Running", "EQI_SYS_STA_ID" : 40.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_RUN", "SUB_ID" : "NA", "SUB_ST" : " ", "OEE_STA_ID" : "NA", "SUB_DESCRP" : " ", "OEE_ITM_STA" : " ", "OEE_ITM_STA_DESCP" : " ", "OEE_SYS_STA_ID" : 0.00, "OEE_SYS_STA_KEY" : " " }, 
+		{ "TOP_ID" : 911124.00, "FQN" : "91AABD", "NAME" : "9111BD", "WORKPLACE_ID" : 1.00, "ACT_ID" : 10001658.00, "ACT_ST" : "2014-10-01 00:00:00.0 +2:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002204.00, "SEG_ST" : "2014-10-01 00:00:00 +02:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : "NA", "SUB_ST" : " ", "OEE_STA_ID" : "NA", "SUB_DESCRP" : " ", "OEE_ITM_STA" : " ", "OEE_ITM_STA_DESCP" : " ", "OEE_SYS_STA_ID" : 0.00, "OEE_SYS_STA_KEY" : " " }, 
+		{ "TOP_ID" : 911131.00, "FQN" : "91AACA", "NAME" : "91AACA", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001701.00, "ACT_ST" : "2014-11-03 15:40:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : "100005552", "REF_ID" : " ", "MAT_NR" : "102339", "MAT_TX" : "Hansa-Brillantgelb 2GX 70 GT", "CHG_ID" : " ", "SEG_ID" : 10002423.00, "SEG_ST" : "2014-11-04 10:05:00 +01:00", "EQI_STA_ID" : 900120.00, "SEG_DESCRP" : "Kein Wasser in Steigleitung A", "EQI_ITM_STA" : "INT", "EQI_ITM_STA_DESCP" : "General Interruption (Don' touch)", "EQI_SYS_STA_ID" : 20.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_INT", "SUB_ID" : 10002106.00, "SUB_ST" : "2014-11-04 10:55:00 +01:00", "OEE_STA_ID" : 912230.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "912230: INT no Material (Don't touch!)", "OEE_ITM_STA_DESCP" : "Interruption due to Logistics", "OEE_SYS_STA_ID" : 223.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_INTL" }, 
+		{ "TOP_ID" : 911139.00, "FQN" : "91AACI", "NAME" : "91AACI", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001498.00, "ACT_ST" : "2014-10-01 00:00:00.0 +2:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002391.00, "SEG_ST" : "2014-10-05 16:30:00 +02:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : "Keiner da?!", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : 10002057.00, "SUB_ST" : "2014-10-05 16:30:00 +02:00", "OEE_STA_ID" : 911230.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "911230: OFF - Staff", "OEE_ITM_STA_DESCP" : "911230: OFF - Staff", "OEE_SYS_STA_ID" : 123.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_DTSR" }, 
+		{ "TOP_ID" : 911151.00, "FQN" : "91AAEA", "NAME" : "91AAEA", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001733.00, "ACT_ST" : "2014-03-01 00:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002352.00, "SEG_ST" : "2014-10-31 22:00:00 +01:00", "EQI_STA_ID" : 900110.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "OFF", "EQI_ITM_STA_DESCP" : "General Shutdown", "EQI_SYS_STA_ID" : 10.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_OFF", "SUB_ID" : "NA", "SUB_ST" : " ", "OEE_STA_ID" : "NA", "SUB_DESCRP" : " ", "OEE_ITM_STA" : " ", "OEE_ITM_STA_DESCP" : " ", "OEE_SYS_STA_ID" : 0.00, "OEE_SYS_STA_KEY" : " " }, 
+		{ "TOP_ID" : 911152.00, "FQN" : "91AAEB", "NAME" : "91AAEB", "WORKPLACE_ID" : 1.00, "ACT_ID" : 10001734.00, "ACT_ST" : "2014-11-01 00:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002374.00, "SEG_ST" : "2014-11-04 08:00:00 +01:00", "EQI_STA_ID" : 900122.00, "SEG_DESCRP" : "E52 T4", "EQI_ITM_STA" : "Logistical INterruption", "EQI_ITM_STA_DESCP" : "LOgistical iNterruption", "EQI_SYS_STA_ID" : 20.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_INT", "SUB_ID" : "NA", "SUB_ST" : " ", "OEE_STA_ID" : "NA", "SUB_DESCRP" : " ", "OEE_ITM_STA" : " ", "OEE_ITM_STA_DESCP" : " ", "OEE_SYS_STA_ID" : 0.00, "OEE_SYS_STA_KEY" : " " }, 
+		{ "TOP_ID" : 918881.00, "FQN" : "91HHHA", "NAME" : "91HHHA", "WORKPLACE_ID" : "NA", "ACT_ID" : 10000742.00, "ACT_ST" : "2014-03-31 08:00:00.0 +2:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10000695.00, "SEG_ST" : "2014-04-03 11:45:00 +02:00", "EQI_STA_ID" : 900130.00, "SEG_DESCRP" : " ", "EQI_ITM_STA" : "INC", "EQI_ITM_STA_DESCP" : "General Incident", "EQI_SYS_STA_ID" : 30.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_INC", "SUB_ID" : "NA", "SUB_ST" : " ", "OEE_STA_ID" : "NA", "SUB_DESCRP" : " ", "OEE_ITM_STA" : " ", "OEE_ITM_STA_DESCP" : " ", "OEE_SYS_STA_ID" : 0.00, "OEE_SYS_STA_KEY" : " " }, 
+		{ "TOP_ID" : 918882.00, "FQN" : "91HHHB", "NAME" : "91HHHB", "WORKPLACE_ID" : "NA", "ACT_ID" : 10001700.00, "ACT_ST" : "2014-11-04 21:00:00.0 +1:00", "ACT_DESCRP" : " ", "AUF_NR" : " ", "REF_ID" : " ", "MAT_NR" : " ", "MAT_TX" : " ", "CHG_ID" : " ", "SEG_ID" : 10002280.00, "SEG_ST" : "2014-11-05 02:00:00 +01:00", "EQI_STA_ID" : 900140.00, "SEG_DESCRP" : "Anlage &quot;AN&gt;&quot;", "EQI_ITM_STA" : "RUN", "EQI_ITM_STA_DESCP" : "Equipment Running", "EQI_SYS_STA_ID" : 40.00, "EQI_SYS_STA_KEY" : "SYS_STA_EQI_RUN", "SUB_ID" : 10002018.00, "SUB_ST" : "2014-11-05 02:00:00 +01:00", "OEE_STA_ID" : 914000.00, "SUB_DESCRP" : " ", "OEE_ITM_STA" : "Running (Don't touch!)", "OEE_ITM_STA_DESCP" : "Running", "OEE_SYS_STA_ID" : 400.00, "OEE_SYS_STA_KEY" : "SYS_STA_OEE_OPER" } ]
+		 } ]
+	 }
+ }; 
